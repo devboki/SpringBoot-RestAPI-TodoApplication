a09b7b507513b90d0881a7b83143b9c4f768c5fe
devboki <octsixxth@gmail.com>
2021-10-14 18:23:11 +0900

    add DB Table, add TodoRepository.java

---
+++ src/main/java/com/example/todo/persistence/TodoRepository.java
@@ -0,0 +1,18 @@
+package com.example.todo.persistence;
+
+import com.example.todo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+//JpaRepository interface 확장. generic type<table entity, primary key type>를 받는다
+
+    @Query("select * from todo t where t.userId = ?1")
+    List<TodoEntity> findByUserId(String userId);
+    //@Query 없어도 JPA가 findByUserId를 파싱해서 SELECT * FROM TodoRepository WHERE userId='{userId}' 쿼리 작성 및 실행
+    //더 복잡한 쿼리는 어노테이션 사용해서 지정해준다
+}
