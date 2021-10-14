a257b0a76a57b6a67cd7ea47a22b3246b58ed2d3
devboki <octsixxth@gmail.com>
2021-10-14 18:24:06 +0900

    add DB Table, add TodoRepository.java

--- src/main/java/com/example/todo/model/TodoEntity.java
+++ src/main/java/com/example/todo/model/TodoEntity.java
@@ -4,14 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder //오브젝트 생성하는 디자인 패턴
+@NoArgsConstructor //디폴트 생성자
+@AllArgsConstructor //모든 변수를 인자로 받는 생성자
+@Data //getter, setter
+@Entity //java class를 entity로 선언
+@Table(name="todo") //class와 table의 이름이 같다면 생략
 public class TodoEntity {
+    @Id //primery key 선언
+    @GeneratedValue(generator = "system-uuid") //system-uuid라는 generator(매개변수)로 id 자동생성
+    @GenericGenerator(name="system-uuid", strategy = "uuid") //generator custom
     private String id;
+
     private String userId;
+
     private String title;
+
     private boolean done; //true - todo checked
 }
--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -1,11 +1,31 @@
 package com.example.todo.service;
 
+import com.example.todo.model.TodoEntity;
+import com.example.todo.persistence.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
-    public String testService(){
-        return "<test service>";
+    @Autowired
+    private TodoRepository repository;
+
+    public String testService() {
+        // TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("my first todo item").build();
+
+        // TodoEntity 저장
+        repository.save(entity);
+
+        // TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+
+        return savedEntity.getTitle();
+        // GET http://localhost:8000/todo/test -> error : null, data : my first todo item
     }
+
+/*    public String testService(){
+        return "<test service>";
+    }*/
 }
